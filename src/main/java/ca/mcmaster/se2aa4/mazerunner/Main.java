1cb1fecf44260f3d1f3b1a88d9f67f2f1c13ca67
Ayush Patel <ayushpatel.2026@gmail.com>
2024-01-17 21:19:56 -0500

    added walking skeleton

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -5,10 +5,61 @@ import java.io.File;
 import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.commons.cli.*;
+import java.util.ArrayList;
 
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
+
+    
+    // Converts the file into a matrix to make the maze easier to work with
+    public static ArrayList<ArrayList<Integer>> matrix(String filename){
+        ArrayList<ArrayList<Integer>> maze = new ArrayList<>();
+        return maze;
+    }
+
+
+    //Determine the coordinates of the entry and exit points
+    public static void entry_exit(ArrayList<ArrayList<Integer>> maze){
+        int[] entry, exit;
+    }
+
+    // Figure out what the next move is based on the right side wall hugging rule
+    public static int[] next_move(int[] current_coordinates){
+        int[] next_coordinates= {0,0};
+        return next_coordinates;
+        
+    }
+
+    /*Check if a move is valid. 
+    Move validity does not depend on whether wall on right side rule is met as we will be implementing new algorithms later
+    Check if index is within range of the maze rows and columns and if the "value" is "PASS"
+    */
+    public static boolean valid(int[] next_coordinates){
+        return true;
+    }
+
+    /*Check if a path is valid, convert long form path into actual coordinate directions, check if a move is valid using
+    the valid method and then calculate and update the new current coordinates 
+    */ 
+    public static int[] pathCheck(int[] current_coordinates, String path){
+        return current_coordinates;
+    }
+
+    /*Take the next coordinates as input and figure out what the path will be for that move
+    Check validity of move first and then append "path" here to a global path variable
+    */
+    public static String directions(int[] next_coordinates){
+        String path = "FFFL";
+        return path;
+    }
+
+
+    // Converting long form of path to factorized form and vice versa
+    public static String pathConverter(String path){
+        return path;
+    }
 
     public static void main(String[] args) {
         logger.info("** Starting Maze Runner");
