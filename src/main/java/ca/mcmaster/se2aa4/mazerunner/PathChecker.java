e5152793a7295c81a65f92dc816da6054425ca6e
Ayush Patel <ayushpatel.2026@gmail.com>
2024-01-24 12:51:23 -0500

    Path verification has been added (close #7)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/PathChecker.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/PathChecker.java
@@ -3,9 +3,27 @@ package ca.mcmaster.se2aa4.mazerunner;
 public class PathChecker {
     MazeRepresenter maze;
     MazeExplorer explorer;
-    PathChecker(MazeRepresenter maze, MazeExplorer explorer){
+    String inputPath;
+    PathChecker(MazeRepresenter maze, MazeExplorer explorer, String inputPath){
         this.maze = maze;
         this.explorer = explorer;
+        this.inputPath = inputPath;
     }
+
+    public boolean isValidPath(String inputPath){
+        Point currentPoint = explorer.currentPoint;
+        MazeExplorer.Direction currentDirection = explorer.intialDirection;
+
+        Point potentialNextPoint = explorer.nextPoint(inputPath, currentPoint, currentDirection);
+        if (potentialNextPoint != null && potentialNextPoint.equals(maze.getExitPoint())){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+
 
 }
